26ca4f4cd56aeeffda3d50bd8329125733358163
Janek Priimann <janek.priimann@gmail.com>
2013-09-10 16:46:22 +0300

    Add placeholder API

---
+++ web/index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var PORT = 1337
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(PORT, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:' + PORT + '/');
