0075d9b42c90a18d1d1d55766b3b6032b1e8d70e
Janek Priimann <janek.priimann@gmail.com>
2013-09-10 21:33:54 +0300

    Update menu controller, add a major workaround to iOS 7 beta SDK bug that makes development impossible

---
+++ ios/Sources/MFMenuController.h
@@ -0,0 +1,13 @@
+//
+//  MFMenuController.h
+//  Reloved
+//
+//  Created by Janek Priimann on 9/10/13.
+//  Copyright (c) 2013 Meep Factory LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFMenuController : NSObject
+
+@end
---
+++ ios/Sources/MFMenuController.m
@@ -0,0 +1,13 @@
+//
+//  MFMenuController.m
+//  Reloved
+//
+//  Created by Janek Priimann on 9/10/13.
+//  Copyright (c) 2013 Meep Factory LLC. All rights reserved.
+//
+
+#import "MFMenuController.h"
+
+@implementation MFMenuController
+
+@end
