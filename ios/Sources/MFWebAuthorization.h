0d14b340cd125898f9faf85b7cdf7f2677adef93
Janek Priimann <janek.priimann@gmail.com>
2013-09-11 15:39:54 +0300

    Implement Web Authorization class

--- ios/Sources/MFWebAuthorization.h
+++ ios/Sources/MFWebAuthorization.h
@@ -2,6 +2,23 @@
 
 #import <Foundation/Foundation.h>
 
+extern NSString *MFWebAuthorizationTypeAutomatic;
+
 @interface MFWebAuthorization : NSObject
+{
+    @private
+    NSString *m_token;
+    NSString *m_secret;
+    NSString *m_type;
+}
+
++ (id)authorizationForDeviceID:(NSString *)deviceId;
+
+- (id)initWithAttributes:(NSDictionary *)attributes;
+
+@property (nonatomic, retain, readonly) NSDictionary *attributes;
+@property (nonatomic, retain, readonly) NSString *token;
+@property (nonatomic, retain, readonly) NSString *secret;
+@property (nonatomic, retain, readonly) NSString *type;
 
 @end
--- ios/Sources/MFWebAuthorization.m
+++ ios/Sources/MFWebAuthorization.m
@@ -1,7 +1,56 @@
 /* Copyright (c) 2013 Meep Factory OU */
 
 #import "MFWebAuthorization.h"
+#import "NSDictionary+Additions.h"
+#import "NSString+Additions.h"
+
+NSString *MFWebAuthorizationTypePlaceholder = @"PA";
+
+#define KEY_TOKEN @"token"
+#define KEY_SECRET @"secret"
+#define KEY_TYPE @"type"
 
 @implementation MFWebAuthorization
+
++ (id)authorizationForDeviceID:(NSString *)deviceId
+{
+    return [[MFWebAuthorization alloc] initWithAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                           MFWebAuthorizationTypePlaceholder, KEY_TYPE, 
+                                                           deviceId.sha1Value, KEY_TOKEN,
+                                                           ((NSString *)[NSString stringWithFormat:@"%@%@", deviceId, deviceId]).sha1Value, KEY_SECRET, nil]];
+}
+
+- (id)initWithAttributes:(NSDictionary *)attributes
+{
+    self = [super init];
+    
+    if(self) {
+        m_token = [attributes stringForKey:KEY_TOKEN];
+        m_secret = [attributes stringForKey:KEY_SECRET];
+        m_type = [attributes stringForKey:KEY_TYPE];
+        
+        if(!m_token || !m_secret || !m_type) {
+            MFError(@"Token, secret and type cannot be nil");
+            
+            return nil;
+        }
+    }
+    
+    return self;
+}
+
+@dynamic attributes;
+
+- (NSDictionary *)attributes
+{
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            m_token, KEY_TOKEN,
+            m_secret, KEY_SECRET,
+            m_type, KEY_TYPE, nil];
+}
+
+@synthesize token = m_token;
+@synthesize secret = m_secret;
+@synthesize type = m_type;
 
 @end
