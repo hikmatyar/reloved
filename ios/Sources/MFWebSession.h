77daaf762b9429885046c997821e861479da1c60
Janek Priimann <janek.priimann@gmail.com>
2013-09-11 15:57:18 +0300

    Implement Web session class

--- ios/Sources/MFWebSession.h
+++ ios/Sources/MFWebSession.h
@@ -3,5 +3,18 @@
 #import <Foundation/Foundation.h>
 
 @interface MFWebSession : NSObject
+{
+    @private
+    NSTimeInterval m_timestamp;
+    NSString *m_identifier;
+    NSString *m_userId;
+}
+
+- (id)initWithAttributes:(NSDictionary *)attributes;
+
+@property (nonatomic, retain, readonly) NSDictionary *attributes;
+@property (nonatomic, assign, readonly) NSTimeInterval timestamp;
+@property (nonatomic, retain, readonly) NSString *identifier;
+@property (nonatomic, retain, readonly) NSString *userId;
 
 @end
--- ios/Sources/MFWebSession.m
+++ ios/Sources/MFWebSession.m
@@ -1,7 +1,49 @@
 /* Copyright (c) 2013 Meep Factory OU */
 
 #import "MFWebSession.h"
+#import "NSDictionary+Additions.h"
+
+#define KEY_IDENTIFIER @"id"
+#define KEY_USERID @"user"
+#define KEY_TIMESTAMP @"timestamp"
 
 @implementation MFWebSession
+
+- (id)initWithAttributes:(NSDictionary *)attributes
+{
+    self = [super init];
+    
+    if(self) {
+        m_identifier = [attributes stringForKey:KEY_IDENTIFIER];
+        m_userId = [attributes stringForKey:KEY_USERID];
+        m_timestamp = [attributes timeIntervalForKey:KEY_TIMESTAMP];
+        
+        if(m_timestamp < 1.0F) {
+            m_timestamp = [NSDate timeIntervalSinceReferenceDate];
+        }
+        
+        if(!m_userId || !m_identifier) {
+            MFError(@"user ID and token must not be nil");
+            
+            return nil;
+        }
+    }
+    
+    return self;
+}
+
+@dynamic attributes;
+
+- (NSDictionary *)attributes
+{
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            m_userId, KEY_USERID,
+            m_identifier, KEY_IDENTIFIER,
+            [NSNumber numberWithDouble:m_timestamp], KEY_TIMESTAMP, nil];
+}
+
+@synthesize timestamp = m_timestamp;
+@synthesize identifier = m_identifier;
+@synthesize userId = m_userId;
 
 @end
