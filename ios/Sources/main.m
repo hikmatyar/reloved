2cea0633e5cbd6f2c20866bc98eb26e1a4d0940e
Janek Priimann <janek.priimann@gmail.com>
2013-09-12 16:11:01 +0300

    main() function hack is not necessary in iOS 7 SDK GM version

--- ios/Sources/main.m
+++ ios/Sources/main.m
@@ -2,29 +2,8 @@
 
 #import "MFApplicationDelegate.h"
 
-// TODO: This blob of code should be removed before submitting to App Store. It's only necessary, because iOS 7 SDK, beta 4+ is completely broken without it.
-// WTF @ Apple. Broken beta has been out in the wild for a month. More info: https://devforums.apple.com/message/861803#861803
-typedef int (*PYStdWriter)(void *, const char *, int);
-
-static PYStdWriter _oldStdWrite;
-
-int __pyStderrWrite(void *inFD, const char *buffer, int size) {
-    if(strncmp(buffer, "AssertMacros:", 13) == 0) {
-        return 0;
-    }
-    
-    return _oldStdWrite(inFD, buffer, size);
-}
-
-void __iOS7B5CleanConsoleOutput(void) {
-    _oldStdWrite = stderr->_write;
-    stderr->_write = __pyStderrWrite;
-}
-
 int main(int argc, char *argv[])
 {
-    __iOS7B5CleanConsoleOutput();
-    
 #if DEBUG
     int result = -1;
     
