ae6a0d093a422f04a309de506cbdab57e83a0ef4
Janek Priimann <janek.priimann@gmail.com>
2013-09-20 05:02:29 +0300

    Basic watcher agent task

--- web/src/apps/agent/tasks/Watcher.hx
+++ web/src/apps/agent/tasks/Watcher.hx
@@ -3,9 +3,38 @@
 package apps.agent.tasks;
 
 import js.Node;
+import models.Post;
 
 class Watcher extends Task {
+    private var min : Int;
+    private var max : Int;
+    
     public function new() {
         super('Watcher', 1000);
+        this.min = -1;
+    }
+    
+    private override function onRun() : Void {
+        if(this.min < 0) {
+            Post.findForward(null, 1, function(err, posts) {
+                if(posts != null && posts.length > 0) {
+                    this.min = posts[0].modified;
+                    this.max = this.min;
+                    this.reschedule(true);
+                } else {
+                    this.reschedule();
+                }
+            });
+        } else {
+            Post.findChanges(this.min, this.max, function(err, changes) {
+                if(changes != null && changes.length > 0) {
+                	// TODO: Launch Stripe task ?!
+                	// TODO: Send emails
+                    trace('detected a change');
+                }
+                
+                this.reschedule();
+            });
+        }
     }
 }
