6479a9a93dc512857008bc5332d59eadd67c330c
Janek Priimann <janek.priimann@gmail.com>
2013-10-08 23:56:05 +0300

    Add Done button automatically if missing to ViewController

--- ios/Sources/Additions/UIViewController+Additions.m
+++ ios/Sources/Additions/UIViewController+Additions.m
@@ -4,12 +4,21 @@
 
 @implementation UIViewController(Additions)
 
+- (IBAction)dismiss:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
 - (void)presentNavigableViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion
 {
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewControllerToPresent];
     
     navigationController.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
     navigationController.navigationBar.translucent = NO;
+    
+    if(!viewControllerToPresent.navigationItem.rightBarButtonItem) {
+        viewControllerToPresent.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismiss:)];
+    }
     
     [self presentViewController:navigationController animated:flag completion:completion];
 }
