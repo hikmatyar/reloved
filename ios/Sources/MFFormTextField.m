3812228d7acdc2e019a09ec59ba9517a12aea51b
Janek Priimann <janek.priimann@gmail.com>
2013-10-08 23:00:07 +0300

    Update FormTextField

--- ios/Sources/MFFormTextField.m
+++ ios/Sources/MFFormTextField.m
@@ -1,15 +1,34 @@
 /* Copyright (c) 2013 Meep Factory OU */
 
 #import "MFFormTextField.h"
+#import "UIColor+Additions.h"
+#import "UIFont+Additions.h"
 
 @implementation MFFormTextField
 
 + (CGFloat)preferredHeight
 {
-    return 50.0F;
+    return 46.0F;
 }
 
 #pragma mark UIView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if(self) {
+        self.backgroundColor = [UIColor themeInputBackgroundColor];
+        self.font = [UIFont themeFontOfSize:14.0F];
+        self.borderStyle = UITextBorderStyleNone;
+        self.textColor = [UIColor themeInputTextColor];
+        self.leftView = [[UIView alloc] initWithFrame:CGRectMake(0.0F, 0.0F, 17.0F, 20.0F)];
+        self.leftViewMode = UITextFieldViewModeAlways;
+        
+    }
+    
+    return self;
+}
 
 
 @end
