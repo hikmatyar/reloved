120facaa3a2e54bd709018092b59c26316f2f0d1
Janek Priimann <janek.priimann@gmail.com>
2013-10-13 23:09:20 +0300

    Update Money model

--- ios/Sources/MFMoney.h
+++ ios/Sources/MFMoney.h
@@ -9,10 +9,12 @@
     NSString *m_currency;
 }
 
+- (id)initWithLocalizedString:(NSString *)str currency:(NSString *)currency;
 - (id)initWithValue:(NSInteger)value currency:(NSString *)currency;
 
 @property (nonatomic, assign, readonly) NSInteger value;
 @property (nonatomic, retain, readonly) NSString *currency;
+@property (nonatomic, retain, readonly) NSString *localizedString;
 @property (nonatomic, retain, readonly) NSString *stringValue;
 
 @end
--- ios/Sources/MFMoney.m
+++ ios/Sources/MFMoney.m
@@ -4,6 +4,17 @@
 
 @implementation MFMoney
 
+- (id)initWithLocalizedString:(NSString *)str currency:(NSString *)currency
+{
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    NSNumber *number;
+    
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    number = [formatter numberFromString:str];
+    
+    return [self initWithValue:round(number.doubleValue * 100.0) currency:currency];
+}
+
 - (id)initWithValue:(NSInteger)value currency:(NSString *)currency
 {
     self = [super init];
@@ -19,13 +30,24 @@
 @synthesize value = m_value;
 @synthesize currency = m_currency;
 
+@dynamic localizedString;
+
+- (NSString *)localizedString
+{
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    
+    return [formatter stringFromNumber:[NSNumber numberWithDouble:(double)m_value / 100.0]];
+}
+
 @dynamic stringValue;
 
 - (NSString *)stringValue
 {
     return (m_value > 10000) ?
-        [NSString stringWithFormat:@"%@ %d.-", (m_currency) ? m_currency : @"", (NSInteger)round((double)m_value / 100.0F)] :
-        [NSString stringWithFormat:@"%@ %g.-", (m_currency) ? m_currency : @"", ((double)m_value / 100.0F)];
+        [NSString stringWithFormat:@"%@ %d.-", (m_currency) ? m_currency : @"", (NSInteger)round((double)m_value / 100.0)] :
+        [NSString stringWithFormat:@"%@ %g.-", (m_currency) ? m_currency : @"", ((double)m_value / 100.0)];
 }
 
 @end
