b2f3d64cd388217d3611d119698ff33889b19f44
Janek Priimann <janek.priimann@gmail.com>
2013-10-18 19:34:36 +0300

    Hide Search view

--- ios/Sources/MFMenuController.m
+++ ios/Sources/MFMenuController.m
@@ -142,7 +142,7 @@
     if(self) {
         m_menu = [[NSArray alloc] initWithObjects:
                   MENU_ITEM(NSLocalizedString(@"Menu.Action.Home", nil), @selector(home:), @"Menu-Home.png"),
-                  MENU_ITEM(NSLocalizedString(@"Menu.Action.Search", nil), @selector(search:), @"Menu-Search.png"),
+                  //MENU_ITEM(NSLocalizedString(@"Menu.Action.Search", nil), @selector(search:), @"Menu-Search.png"),
                   MENU_ITEM(NSLocalizedString(@"Menu.Action.News", nil), @selector(news:), @"Menu-News.png"),
                   MENU_ITEM(NSLocalizedString(@"Menu.Action.Cart", nil), @selector(cart:), @"Menu-Cart.png"),
                   MENU_ITEM(NSLocalizedString(@"Menu.Action.Sell", nil), @selector(sell:), @"Menu-Sell.png"), nil];
