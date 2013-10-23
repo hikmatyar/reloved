101a9e0d1042542e489dd3c5a6cd12c419e0a7fb
Janek Priimann <janek.priimann@gmail.com>
2013-10-23 23:05:31 +0300

    Update Cart model

--- ios/Sources/MFCart.h
+++ ios/Sources/MFCart.h
@@ -5,17 +5,61 @@
 @interface MFCart : NSObject
 {
     @protected
+    NSString *m_email;
+    NSString *m_phone;
+    NSString *m_currency;
+    NSInteger m_price;
+    NSInteger m_amount;
+    NSInteger m_transactionFee;
+    NSString *m_deliveryId;
+    NSString *m_firstName;
+    NSString *m_lastName;
+    NSString *m_countryId;
+    NSString *m_city;
+    NSString *m_address;
+    NSString *m_zipcode;
     NSArray *m_postIds;
+    NSString *m_stripeToken;
 }
 
 @property (nonatomic, retain, readonly) NSDictionary *attributes;
 
+@property (nonatomic, assign, readonly, getter = isEmpty) BOOL empty;
 @property (nonatomic, retain, readonly) NSArray *postIds;
+@property (nonatomic, retain, readonly) NSString *deliveryId;
+@property (nonatomic, retain, readonly) NSString *currency;
+@property (nonatomic, assign, readonly) NSInteger price;
+@property (nonatomic, assign, readonly) NSInteger amount;
+@property (nonatomic, assign, readonly) NSInteger transactionFee;
+@property (nonatomic, retain, readonly) NSString *fullName;
+@property (nonatomic, retain, readonly) NSString *email;
+@property (nonatomic, retain, readonly) NSString *phone;
+@property (nonatomic, retain, readonly) NSString *firstName;
+@property (nonatomic, retain, readonly) NSString *lastName;
+@property (nonatomic, retain, readonly) NSString *countryId;
+@property (nonatomic, retain, readonly) NSString *city;
+@property (nonatomic, retain, readonly) NSString *address;
+@property (nonatomic, retain, readonly) NSString *zipcode;
+@property (nonatomic, retain, readonly) NSString *stripeToken;
 
 @end
 
 @interface MFMutableCart : MFCart
 
 @property (nonatomic, retain) NSArray *postIds;
+@property (nonatomic, retain) NSString *email;
+@property (nonatomic, retain) NSString *phone;
+@property (nonatomic, retain) NSString *deliveryId;
+@property (nonatomic, retain) NSString *currency;
+@property (nonatomic, assign) NSInteger amount;
+@property (nonatomic, assign) NSInteger price;
+@property (nonatomic, assign) NSInteger transactionFee;
+@property (nonatomic, retain) NSString *firstName;
+@property (nonatomic, retain) NSString *lastName;
+@property (nonatomic, retain) NSString *countryId;
+@property (nonatomic, retain) NSString *city;
+@property (nonatomic, retain) NSString *address;
+@property (nonatomic, retain) NSString *zipcode;
+@property (nonatomic, retain) NSString *stripeToken;
 
 @end
--- ios/Sources/MFCart.m
+++ ios/Sources/MFCart.m
@@ -2,6 +2,21 @@
 
 #import "MFCart.h"
 
+#define KEY_DELIVERY @"delivery"
+#define KEY_STRIPE @"stripe"
+#define KEY_PRICE @"price"
+#define KEY_AMOUNT @"amount"
+#define KEY_TRANSACTION_FEE @"fee"
+#define KEY_CURRENCY @"currency"
+#define KEY_EMAIL @"email"
+#define KEY_PHONE @"phone"
+#define KEY_FIRST_NAME @"first_name"
+#define KEY_LAST_NAME @"last_name"
+#define KEY_COUNTRY @"country"
+#define KEY_CITY @"city"
+#define KEY_ADDRESS @"address"
+#define KEY_ZIPCODE @"zipcode"
+
 @implementation MFCart
 
 @dynamic attributes;
@@ -10,12 +25,52 @@
 {
     NSMutableDictionary *attributes = [[NSMutableDictionary alloc] init];
     
-    
+    [attributes setValue:m_deliveryId forKey:KEY_DELIVERY];
+    [attributes setValue:m_stripeToken forKey:KEY_STRIPE];
+    [attributes setValue:[NSNumber numberWithInteger:m_price] forKey:KEY_PRICE];
+    [attributes setValue:[NSNumber numberWithInteger:m_amount] forKey:KEY_AMOUNT];
+    [attributes setValue:[NSNumber numberWithInteger:m_transactionFee] forKey:KEY_TRANSACTION_FEE];
+    [attributes setValue:m_currency forKey:KEY_CURRENCY];
+    [attributes setValue:m_email forKey:KEY_EMAIL];
+    [attributes setValue:m_phone forKey:KEY_PHONE];
+    [attributes setValue:m_firstName forKey:KEY_FIRST_NAME];
+    [attributes setValue:m_lastName forKey:KEY_LAST_NAME];
+    [attributes setValue:m_countryId forKey:KEY_COUNTRY];
+    [attributes setValue:m_city forKey:KEY_CITY];
+    [attributes setValue:m_address forKey:KEY_ADDRESS];
+    [attributes setValue:m_zipcode forKey:KEY_ZIPCODE];
     
     return attributes;
 }
 
+@dynamic empty;
+
+- (BOOL)isEmpty
+{
+    return (self.attributes.count == 0) ? YES : NO;
+}
+
+@dynamic fullName;
+
+- (NSString *)fullName
+{
+    return (m_firstName.length > 0 && m_lastName.length > 0) ? [NSString stringWithFormat:@"%@ %@", m_firstName, m_lastName] : ((m_firstName.length > 0) ? m_firstName : m_lastName);
+}
+
 @synthesize postIds = m_postIds;
+@synthesize email = m_email;
+@synthesize phone = m_phone;
+@synthesize deliveryId = m_deliveryId;
+@synthesize price = m_price;
+@synthesize amount = m_amount;
+@synthesize transactionFee = m_transactionFee;
+@synthesize firstName = m_firstName;
+@synthesize lastName = m_lastName;
+@synthesize countryId = m_countryId;
+@synthesize city = m_city;
+@synthesize address = m_address;
+@synthesize zipcode = m_zipcode;
+@synthesize stripeToken = m_stripeToken;
 
 @end
 
@@ -26,6 +81,76 @@
 - (void)setPostIds:(NSArray *)postIds
 {
     m_postIds = postIds;
+}
+
+- (void)setDeliveryId:(NSString *)deliveryId
+{
+    m_deliveryId = deliveryId;
+}
+
+- (void)setCurrency:(NSString *)currency
+{
+    m_currency = currency;
+}
+
+- (void)setAmount:(NSInteger)amount
+{
+    m_amount = amount;
+}
+
+- (void)setPrice:(NSInteger)price
+{
+    m_price = price;
+}
+
+- (void)setTransactionFee:(NSInteger)transactionFee
+{
+    m_transactionFee = transactionFee;
+}
+
+- (void)setEmail:(NSString *)email
+{
+    m_email = email;
+}
+
+- (void)setPhone:(NSString *)phone
+{
+    m_phone = phone;
+}
+
+- (void)setFirstName:(NSString *)firstName
+{
+    m_firstName = firstName;
+}
+
+- (void)setLastName:(NSString *)lastName
+{
+    m_lastName = lastName;
+}
+
+- (void)setCountryId:(NSString *)countryId
+{
+    m_countryId = countryId;
+}
+
+- (void)setCity:(NSString *)city
+{
+    m_city = city;
+}
+
+- (void)setAddress:(NSString *)address
+{
+    m_address = address;
+}
+
+- (void)setZipcode:(NSString *)zipcode
+{
+    m_zipcode = zipcode;
+}
+
+- (void)setStripeToken:(NSString *)stripeToken
+{
+    m_stripeToken = stripeToken;
 }
 
 @end
