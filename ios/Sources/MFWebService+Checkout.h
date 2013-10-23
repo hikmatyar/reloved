d102c65f45a857ae9f673e191f6fcb9ab3716477
Janek Priimann <janek.priimann@gmail.com>
2013-10-23 15:14:50 +0300

    Update WebService (Checkout)

--- ios/Sources/MFWebService+Checkout.h
+++ ios/Sources/MFWebService+Checkout.h
@@ -3,6 +3,12 @@
 #import "MFWebRequest.h"
 #import "MFWebService.h"
 
+@class MFCart;
+
 @interface MFWebService(Checkout)
+
+- (void)requestCheckout:(NSArray *)postIds lastModification:(NSDate *)lastModification target:(id)target usingBlock:(MFWebRequestBlock)block;
+- (void)requestOrderCreate:(MFCart *)cart target:(id)target usingBlock:(MFWebRequestBlock)block;
+- (void)requestOrderStatus:(NSString *)orderId target:(id)target usingBlock:(MFWebRequestBlock)block;
 
 @end
--- ios/Sources/MFWebService+Checkout.m
+++ ios/Sources/MFWebService+Checkout.m
@@ -1,7 +1,53 @@
 /* Copyright (c) 2013 Meep Factory OU */
 
+#import "MFCart.h"
+#import "MFCheckout.h"
+#import "MFOrder.h"
 #import "MFWebService+Checkout.h"
+#import "NSDate+Additions.h"
 
 @implementation MFWebService(Checkout)
+
+- (void)requestCheckout:(NSArray *)postIds lastModification:(NSDate *)lastModification target:(id)target usingBlock:(MFWebRequestBlock)block
+{
+    MFWebRequest *request = [[MFWebRequest alloc] initWithService:self
+                                                             mode:kMFWebRequestModeJsonPost
+                                                           target:target
+                                                             path:@"/checkout"
+                                                       parameters:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                                  postIds, @"ids",
+                                                                  (lastModification) ? lastModification.datetimeString : [NSNull null], @"mod", nil]];
+    
+    request.block = block;
+    request.transform = [MFCheckout class];
+    [self addRequest:request];
+}
+
+- (void)requestOrderCreate:(MFCart *)cart target:(id)target usingBlock:(MFWebRequestBlock)block
+{
+    MFWebRequest *request = [[MFWebRequest alloc] initWithService:self
+                                                             mode:kMFWebRequestModeJsonPost
+                                                           target:target
+                                                             path:@"/checkout/commit"
+                                                       parameters:cart.attributes];
+    
+    request.block = block;
+    request.transform = [MFOrder class];
+    [self addRequest:request];
+}
+
+- (void)requestOrderStatus:(NSString *)orderId target:(id)target usingBlock:(MFWebRequestBlock)block
+{
+    MFWebRequest *request = [[MFWebRequest alloc] initWithService:self
+                                                             mode:kMFWebRequestModeJsonPost
+                                                           target:target
+                                                             path:@"/checkout/status"
+                                                       parameters:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                                  orderId, @"id", nil]];
+    
+    request.block = block;
+    request.transform = [MFOrder class];
+    [self addRequest:request];
+}
 
 @end
