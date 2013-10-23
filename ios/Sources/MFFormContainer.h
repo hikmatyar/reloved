a15163bed78cdf867baca09029ca1878cbb29c16
Janek Priimann <janek.priimann@gmail.com>
2013-10-24 00:21:34 +0300

    Add FormContainer class

---
+++ ios/Sources/MFFormContainer.h
@@ -0,0 +1,7 @@
+/* Copyright (c) 2013 Meep Factory OU */
+
+#import "MFForm.h"
+
+@interface MFFormContainer : UIView <MFFormElement>
+
+@end
---
+++ ios/Sources/MFFormContainer.m
@@ -0,0 +1,7 @@
+/* Copyright (c) 2013 Meep Factory OU */
+
+#import "MFFormContainer.h"
+
+@implementation MFFormContainer
+
+@end
