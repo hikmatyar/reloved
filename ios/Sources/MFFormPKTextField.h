e34f6286fc207bf2e6ebe64fe8a7afee3ae25910
Janek Priimann <janek.priimann@gmail.com>
2013-10-27 22:31:49 +0200

    Add FormPKTextField class

---
+++ ios/Sources/MFFormPKTextField.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2013 Meep Factory OU */
+
+#import "MFForm.h"
+#import "PKTextField.h"
+
+@interface MFFormPKTextField : PKTextField <MFFormElement>
+
++ (CGFloat)preferredHeight;
+
+@end
