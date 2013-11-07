5b18c39eebd22f706cbdb7e4154a71a40b3ec477
Janek Priimann <janek.priimann@gmail.com>
2013-11-07 21:27:53 +0200

    Add explicit image resizing

--- ios/Sources/Additions/UIImage+Additions.m
+++ ios/Sources/Additions/UIImage+Additions.m
@@ -31,6 +31,7 @@
         CGFloat r2 = maxSize.height / oldSize.height;
         CGFloat r = (r1 < r2) ? r1 : r2;
         CGRect newRect = CGRectIntegral(CGRectMake(0.0F, 0.0F, oldSize.width * r, oldSize.height * r));
+        CGRect transposedRect = CGRectMake(0.0F, 0.0F, newRect.size.height, newRect.size.width);
         CGImageRef imageRef = self.CGImage, newImageRef;
         CGContextRef bitmap = CGBitmapContextCreate(NULL,
                                                     newRect.size.width,
@@ -39,13 +40,63 @@
                                                     0,
                                                     CGImageGetColorSpace(imageRef),
                                                     CGImageGetBitmapInfo(imageRef));
+        CGAffineTransform transform = CGAffineTransformIdentity;
+        UIImageOrientation orientation = self.imageOrientation;
         UIImage *newImage;
+        BOOL transpose;
+        
+        switch(orientation) {
+            case UIImageOrientationLeft:
+            case UIImageOrientationLeftMirrored:
+            case UIImageOrientationRight:
+            case UIImageOrientationRightMirrored:
+                transpose = YES;
+                break;
+            default:
+                transpose = NO;
+        }
+        
+        switch(orientation) {
+            case UIImageOrientationDown:           // EXIF = 3
+            case UIImageOrientationDownMirrored:   // EXIF = 4
+                transform = CGAffineTransformTranslate(transform, newRect.size.width, newRect.size.height);
+                transform = CGAffineTransformRotate(transform, M_PI);
+                break;
+            case UIImageOrientationLeft:           // EXIF = 6
+            case UIImageOrientationLeftMirrored:   // EXIF = 5
+                transform = CGAffineTransformTranslate(transform, newRect.size.width, 0.0F);
+                transform = CGAffineTransformRotate(transform, M_PI_2);
+                break;
+            case UIImageOrientationRight:          // EXIF = 8
+            case UIImageOrientationRightMirrored:  // EXIF = 7
+                transform = CGAffineTransformTranslate(transform, 0.0F, newRect.size.height);
+                transform = CGAffineTransformRotate(transform, -M_PI_2);
+                break;
+            default:
+                break;
+        }
+        
+        switch(orientation) {
+            case UIImageOrientationUpMirrored:     // EXIF = 2
+            case UIImageOrientationDownMirrored:   // EXIF = 4
+                transform = CGAffineTransformTranslate(transform, newRect.size.width, 0.0F);
+                transform = CGAffineTransformScale(transform, -1.0F, 1.0F);
+                break;
+            case UIImageOrientationLeftMirrored:   // EXIF = 5
+            case UIImageOrientationRightMirrored:  // EXIF = 7
+                transform = CGAffineTransformTranslate(transform, newRect.size.height, 0.0F);
+                transform = CGAffineTransformScale(transform, -1.0F, 1.0F);
+                break;
+            default:
+                break;
+        }
         
         CGContextSetInterpolationQuality(bitmap, quality);
-        CGContextDrawImage(bitmap, newRect, imageRef);
+        CGContextConcatCTM(bitmap, transform);
+        CGContextDrawImage(bitmap, (transpose) ? transposedRect : newRect, imageRef);
         
         newImageRef = CGBitmapContextCreateImage(bitmap);
-        newImage = [UIImage imageWithCGImage:newImageRef];
+        newImage = [UIImage imageWithCGImage:newImageRef scale:self.scale orientation:UIImageOrientationUp];
         
         CGContextRelease(bitmap);
         CGImageRelease(newImageRef);
