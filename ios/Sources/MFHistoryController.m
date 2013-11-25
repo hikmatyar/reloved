ddf2d3023f86084d550c6f764deb12740cedb1f0
Janek Priimann <janek.priimann@gmail.com>
2013-11-26 01:33:34 +0200

    More UX changes

--- ios/Sources/MFHistoryController.m
+++ ios/Sources/MFHistoryController.m
@@ -1,9 +1,11 @@
 /* Copyright (c) 2013 Meep Factory OU */
 
 #import "MFHistoryController.h"
+#import "MFProfileController.h"
 #import "MFSideMenuContainerViewController.h"
 #import "MFTableView.h"
 #import "MFWebFeed.h"
+#import "UIViewController+Additions.h"
 #import "UIViewController+MFSideMenuAdditions.h"
 
 @implementation MFHistoryController
@@ -11,6 +13,11 @@
 - (IBAction)menu:(id)sender
 {
     [self.menuContainerViewController toggleLeftSideMenuCompletion:NULL];
+}
+
+- (IBAction)profile:(id)sender
+{
+    [self presentNavigableViewController:[[MFProfileController alloc] init] animated:YES completion:NULL];
 }
 
 #pragma mark UIViewController
@@ -29,6 +36,7 @@
     
     if(self) {
         self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Navigation-Menu"] style:UIBarButtonItemStyleBordered target:self action:@selector(menu:)];
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Navigation-Profile"] style:UIBarButtonItemStyleBordered target:self action:@selector(profile:)];
         self.title = NSLocalizedString(@"History.Title", nil);
     }
     
